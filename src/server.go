4a86aecc8c9b19fcc70e5c4bf599f16aa07aa03d
Siddharth Kumar <siddharth.kumar@rivigo.com>
2019-09-12 17:28:55 +0530

    added methods to add and get users

--- src/server.go
+++ src/server.go
@@ -4,10 +4,33 @@ import (
     "fmt"
     "net/http"
     "github.com/gorilla/mux"
+    "database/sql"
+    "log"
+    "crypto/sha1"
+    "encoding/hex"
+    "time"
+
+    _ "github.com/go-sql-driver/mysql"
 )
+
+type user struct {
+    id        int
+    phoneNumber  string
+    name  string
+    role  string
+}
 
 func main() {
     r := mux.NewRouter()
+    db, err := sql.Open("mysql", "root:root@(127.0.0.1:3306)/gir_rakshak?parseTime=true")
+    
+    if err != nil {
+        log.Fatal(err)
+    }
+    if err := db.Ping(); err != nil {
+        log.Fatal(err)
+    }
+
 
     r.HandleFunc("/users/{id}/locations/{from-date}/{to-date}", func(w http.ResponseWriter, r *http.Request) {
         vars := mux.Vars(r)
@@ -16,7 +39,57 @@ func main() {
         toDate := vars["to-date"]
 
         fmt.Fprintf(w, "You've requested the user's location: %s from-date %s and to-date %s\n", id, fromDate, toDate)
-    })
+    }).Methods("GET")
+
+
+    r.HandleFunc("/users/", func(w http.ResponseWriter, r *http.Request) {
+        
+        rows, err := db.Query(`SELECT id, phone_number, name, role FROM user`)
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer rows.Close()
+
+        var users []user
+        for rows.Next() {
+            var u user
+
+            err := rows.Scan(&u.id, &u.phoneNumber, &u.name, &u.role)
+            if err != nil {
+                log.Fatal(err)
+            }
+            users = append(users, u)
+        }
+        if err := rows.Err(); err != nil {
+            log.Fatal(err)
+        }
+
+        fmt.Fprintf(w, "%#v", users)        
+
+//        fmt.Fprintf(w, "You've requested the user's location: %s from-date %s and to-date %s\n", id, fromDate, toDate)
+    }).Methods("GET")
+
+
+    r.HandleFunc("/users/{name}/{phone-number}/{role}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        name := vars["name"]
+        phoneNumber := vars["phone-number"]
+        role := vars["role"]
+        h := sha1.New()
+        h.Write([]byte(phoneNumber))
+        password := hex.EncodeToString(h.Sum(nil))
+        currTime := time.Now().Unix()
+
+        result, err := db.Exec(`INSERT INTO user (phone_number, name, password, role, created_date, last_updated_date) VALUES (?, ?, ?, ?, ?, ?)`, phoneNumber, name, password, role, currTime, currTime)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        id, err := result.LastInsertId()
+
+        fmt.Fprintf(w, "You've added a new user with id %s phone %s name and role %s\n", id, phoneNumber, name, role)
+    }).Methods("POST")
+
 
     r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
