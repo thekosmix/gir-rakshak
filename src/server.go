071f44056afeb2e45871390228ea35a4bfe57ea2
Siddharth Kumar <siddharth.kumar@rivigo.com>
2019-09-12 16:24:07 +0530

    added mux

--- src/server.go
+++ src/server.go
@@ -3,15 +3,27 @@ package main
 import (
     "fmt"
     "net/http"
+    "github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    r := mux.NewRouter()
+
+    r.HandleFunc("/users/{id}/locations/{from-date}/{to-date}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        id := vars["id"]
+        fromDate := vars["from-date"]
+        toDate := vars["to-date"]
+
+        fmt.Fprintf(w, "You've requested the user's location: %s from-date %s and to-date %s\n", id, fromDate, toDate)
+    })
+
+    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
     })
 
     fs := http.FileServer(http.Dir("static/"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+    r.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    http.ListenAndServe(":80", nil)
+    http.ListenAndServe(":80", r)
 }
