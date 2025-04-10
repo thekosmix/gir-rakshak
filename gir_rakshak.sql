2dd150b5111fd88f7a7596d782c183eb8968c093
Siddharth Kumar <siddharth.dce08@gmail.com>
2025-04-11 00:50:19 +0530

    Update gir_rakshak.sql

--- gir_rakshak.sql
+++ gir_rakshak.sql
@@ -15,6 +15,51 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `phone_number` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  `device_id` varchar(255) NOT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '0',
+  `created_date` bigint NOT NULL,
+  `last_updated_date` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phone_number_UNIQUE` (`phone_number`),
+  UNIQUE KEY `device_id_UNIQUE` (`device_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Dheeraj','9643306341','949e3290016227e97911273929e6a7dc6507cee4','ADMIN','fdbhjdfhbjdfkj',1,1638516009179,1638518609789),(2,'Ravi','8854306341','be459340ddb9c8286fc08bebdce70ca287e03a1a','FIELD','e2fdbyteryuhjdfhbjdfkj',1,1638516048158,1638518614866),(3,'Manish','9954306341','9c8286fc08bebe459340ddbbdce70ca287e03a1a','FIELD','d5fdbhjdfhbjdfkj',0,1638519058912,1638519058912),(8,'Rahul','7754306341','9c8286fc08bebe459340ddbbdce70ca287e03a1a','FIELD','z15fdbhjdfhbjdfkj',1,1638519287690,1638519395180),(11,'Salem','6654306341','9c8286fc08bebe459340ddbbdce70ca287e03a1a','FIELD','t15fdbhjdfhbjdfkj',0,1638519318803,1638519318803);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-03 15:57:19
+
 --
 -- Table structure for table `activity`
 --
@@ -33,7 +78,7 @@ CREATE TABLE `activity` (
   PRIMARY KEY (`id`),
   KEY `user_id_idx` (`user_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,7 +108,7 @@ CREATE TABLE `location` (
   PRIMARY KEY (`id`),
   KEY `user_fk_idx` (`user_id`),
   CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -76,46 +121,32 @@ INSERT INTO `location` VALUES (1,2,'28.74','72.85',15464878732,1638518949939),(2
 /*!40000 ALTER TABLE `location` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `user`
---
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `activity_detail`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
+CREATE TABLE `activity_detail` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `phone_number` varchar(45) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` varchar(45) NOT NULL,
-  `device_id` varchar(255) NOT NULL,
-  `is_active` tinyint NOT NULL DEFAULT '0',
+  `user_id` int NOT NULL,
+  `activity_id` int NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `image_url` varchar(255) NOT NULL,
+  `lat` varchar(45) NOT NULL,
+  `lon` varchar(45) NOT NULL,
+  `recorded_time` bigint NOT NULL,
   `created_date` bigint NOT NULL,
-  `last_updated_date` bigint NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `phone_number_UNIQUE` (`phone_number`),
-  UNIQUE KEY `device_id_UNIQUE` (`device_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `activity`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Dheeraj','9643306341','949e3290016227e97911273929e6a7dc6507cee4','ADMIN','fdbhjdfhbjdfkj',1,1638516009179,1638518609789),(2,'Ravi','8854306341','be459340ddb9c8286fc08bebdce70ca287e03a1a','FIELD','e2fdbyteryuhjdfhbjdfkj',1,1638516048158,1638518614866),(3,'Manish','9954306341','9c8286fc08bebe459340ddbbdce70ca287e03a1a','FIELD','d5fdbhjdfhbjdfkj',0,1638519058912,1638519058912),(8,'Rahul','7754306341','9c8286fc08bebe459340ddbbdce70ca287e03a1a','FIELD','z15fdbhjdfhbjdfkj',1,1638519287690,1638519395180),(11,'Salem','6654306341','9c8286fc08bebe459340ddbbdce70ca287e03a1a','FIELD','t15fdbhjdfhbjdfkj',0,1638519318803,1638519318803);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+LOCK TABLES `activity` WRITE;
+/*!40000 ALTER TABLE `activity` DISABLE KEYS */;
+INSERT INTO `activity` VALUES (1,8,'hello world',1562563647,'24.75','72.45',1638524237631),(2,8,'hello world',1562563647,'24.75','72.45',1638525712405);
+/*!40000 ALTER TABLE `activity` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-12-03 15:57:19
